98c623e0bd127b99d46f32ee830406127a012064
cem.karaoguz@gmail.com <cem.karaoguz@gmail.com>
2020-04-23 01:59:28 +0200

    implemented timeline drawable

--- app/src/main/java/com/bilgekod/naplog/DailyNapAdapter.java
+++ app/src/main/java/com/bilgekod/naplog/DailyNapAdapter.java
@@ -1,12 +1,16 @@
 package com.bilgekod.naplog;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -49,12 +53,14 @@ public class DailyNapAdapter extends RecyclerView.Adapter<DailyNapAdapter.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder
     {
         TextView tvDailyInfo;
+        ImageView ivTimeline;
 
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
 
             tvDailyInfo = itemView.findViewById(R.id.tvDailyInfo);
+            ivTimeline = itemView.findViewById(R.id.ivTimeline);
 
             itemView.setOnClickListener(new View.OnClickListener()
             {
@@ -86,7 +92,41 @@ public class DailyNapAdapter extends RecyclerView.Adapter<DailyNapAdapter.ViewHo
             sMinutes = sMinutes + "s";
         }
         holder.itemView.setTag(dailyNaps.get(position));
-        holder.tvDailyInfo.setText("Total nap duration on " + dailyNaps.get(position).getDateStart() + " is " + duration + sMinutes);
+        //holder.tvDailyInfo.setText("Total nap duration on " + dailyNaps.get(position).getDateStart() + " is " + duration + sMinutes);
+        holder.tvDailyInfo.setText(dailyNaps.get(position).getDateStart());
+
+
+        float hourMin = 8*60f; // TODO to globals
+        float hourMax = 20*60f; // TODO to globals
+
+        ArrayList<Nap> list_nap = dailyNaps.get(position).getNapList();
+        ArrayList<Float> list_hours = new ArrayList<>();
+        ArrayList<Float> list_durations = new ArrayList<>();
+
+        for(int i=0; i<list_nap.size();i++)
+        {
+            list_hours.add(Float.parseFloat(list_nap.get(i).getTime_start_hour())*60f + Float.parseFloat(list_nap.get(i).getTime_start_minute()));
+            list_durations.add(Float.parseFloat(list_nap.get(i).getDuration()));
+        }
+
+        try
+        {
+            //TODO colors
+            TimelineDrawable timelineDrawable = new TimelineDrawable(Color.CYAN,
+                    Color.RED,
+                    Color.WHITE,
+                    hourMin, hourMax, list_hours, list_durations);
+
+            //timelineDrawable.setDoShowDurations(doShowDurations);
+
+            //rectanglesDrawable.setAlpha(80);
+
+            holder.ivTimeline.setImageDrawable(timelineDrawable);
+        }
+        catch(Exception e)
+        {
+            //Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/bilgekod/naplog/TimelineDrawable.java
@@ -0,0 +1,173 @@
+package com.bilgekod.naplog;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class TimelineDrawable extends Drawable
+{
+    private Paint paint_bg;
+    private Paint paint_fg;
+    private Paint paint_text;
+
+    private final float textSizeMin = 10f;
+    private final float textSizeMax = 60f;
+    private float hourMax;
+    private float hourMin;
+
+    private ArrayList<Float> list_hours;
+    private ArrayList<Float> list_durations;
+
+    private int flagAboveBelow;
+    private boolean doShowDurations;
+
+    public TimelineDrawable(int color_fg, int color_bg, int color_text, float hourMin, float hourMax, ArrayList<Float> list_hours, ArrayList<Float> list_durations) throws Exception
+    {
+        paint_fg = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint_fg.setColor(color_fg);
+
+        paint_bg = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint_bg.setColor(color_bg);
+
+        paint_text = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint_text.setColor(color_text);
+
+        this.hourMin = hourMin;
+        this.hourMax = hourMax;
+
+        if(hourMin>=hourMax)
+        {
+            throw new Exception("hourMax must be bigger than hourMin");
+        }
+
+        this.list_hours = list_hours; //TODO check size equals
+        this.list_durations = list_durations;
+
+        if(list_hours.size()!=list_durations.size())
+        {
+            throw new Exception("hour and duration array size mismatch");
+        }
+
+        doShowDurations = true;
+        flagAboveBelow = 1;
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas)
+    {
+        Rect bounds = getBounds();
+
+        canvas.drawRect(bounds.left, bounds.top, bounds.right, bounds.bottom, paint_bg);
+
+        float hour;
+        float duration;
+        int i;
+
+        for(i=0; i<list_hours.size(); i++)
+        {
+            hour = list_hours.get(i);
+            duration = list_durations.get(i);
+
+            if ((hour + duration) < hourMin || hour > hourMax)
+            {
+                // out of bounds
+                continue;
+            }
+            else
+            {
+                float nap_left = Math.max((hour - hourMin) * ((bounds.right - bounds.left) / (hourMax - hourMin)) + bounds.left, bounds.left);
+                float nap_right = Math.min((hour - hourMin + duration) * ((bounds.right - bounds.left) / (hourMax - hourMin)) + bounds.left, bounds.right);
+
+                canvas.drawRect(nap_left, bounds.top, nap_right, bounds.bottom, paint_fg);
+            }
+        }
+
+        for(i=0; i<list_hours.size(); i++)
+        {
+            hour = list_hours.get(i);
+            duration = list_durations.get(i);
+
+            if ((hour + duration) < hourMin || hour > hourMax)
+            {
+                // out of bounds
+                continue;
+            }
+            else
+            {
+                float nap_left = Math.max((hour - hourMin) * ((bounds.right - bounds.left) / (hourMax - hourMin)) + bounds.left, bounds.left);
+
+                float textSize = (bounds.bottom - bounds.top) * 0.3f;
+                if (textSize < textSizeMin)
+                {
+                    textSize = textSizeMin;
+                }
+                if (textSize > textSizeMax)
+                {
+                    textSize = textSizeMax;
+                }
+                paint_text.setTextSize(textSize);
+
+                String hour_str = (int) (hour / 60) + "h";
+                String minute_str = "" + (int) (hour % 60);
+                if (minute_str.length() == 1)
+                {
+                    minute_str = "0" + minute_str;
+                }
+
+                float text_y;
+                if (flagAboveBelow > 0)
+                {
+                    text_y = bounds.top + (bounds.bottom - bounds.top) * 0.4f;
+                } else
+                {
+                    text_y = bounds.top + (bounds.bottom - bounds.top) * 0.8f;
+                }
+
+                if(doShowDurations)
+                {
+                    canvas.drawText((int) (duration) + " mins", nap_left, text_y, paint_text);
+                }
+                else
+                {
+                    canvas.drawText(hour_str + minute_str, nap_left, text_y, paint_text);
+                }
+
+                flagAboveBelow = flagAboveBelow * -1;
+            }
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha)
+    {
+        paint_fg.setAlpha(alpha);
+        paint_bg.setAlpha(alpha);
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter)
+    {
+        paint_fg.setColorFilter(colorFilter);
+        paint_bg.setColorFilter(colorFilter);
+    }
+
+    @Override
+    public int getOpacity()
+    {
+        //give the desired opacity of the shape
+        return PixelFormat.TRANSLUCENT;
+    }
+
+    public void setDoShowDurations(boolean newDoShowDurations)
+    {
+        doShowDurations = newDoShowDurations;
+    }
+}
